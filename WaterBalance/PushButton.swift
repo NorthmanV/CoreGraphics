4a796995063fac99a5a285f3bf5bc2ac718847f8
Ruslan Akberov <ruslanakberov@gmail.com>
2018-10-25 23:21:05 +0300

    Button with white line created

--- WaterBalance/PushButton.swift
+++ WaterBalance/PushButton.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class PushButton: UIButton {
     
+    private struct Constants {
+        static let plusLineWidth: CGFloat = 3.0
+        static let plusButtonScale: CGFloat = 0.6
+        static let halfPointShift: CGFloat = 0.5
+    }
+    
+    private var halfWidth: CGFloat {
+        return bounds.width / 2
+    }
+    
+    private var halfHeight: CGFloat {
+        return bounds.height / 2
+    }
+    
     override func draw(_ rect: CGRect) {
         let path = UIBezierPath(ovalIn: rect)
-        UIColor.green.setFill()
+        UIColor.blue.setFill()
         path.fill()
+        
+        let plusWidth: CGFloat = min(bounds.width, bounds.height) * Constants.plusButtonScale
+        let halfPlusWidth: CGFloat = plusWidth / 2
+        let plusPath = UIBezierPath()
+        plusPath.lineWidth = Constants.plusLineWidth
+        plusPath.move(to: CGPoint(x: halfWidth - halfPlusWidth, y: halfHeight))
+        plusPath.addLine(to: CGPoint(x: halfWidth + halfPlusWidth, y: halfHeight))
+        UIColor.white.setStroke()
+        plusPath.stroke()
     }
+
 }
